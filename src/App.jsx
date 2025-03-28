0f32e04afd52e5ab0b129179f01ac3f83072e7de
Dhruv Bajoria <dhruvbajoria2001@gmail.com>
2025-03-28 17:07:23 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Hero from './components/Hero';
-import About from './components/about';
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/about";
+import NavBar from "./components/Navbar";
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <Hero/>
-      <About/>
-      <section className='z-0 min-h-screen bg-blue-500'></section>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <section className="z-0 min-h-screen bg-blue-500"></section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
